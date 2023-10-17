e43d0953804217956101dc39ec3f6de2a5b488fc
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-10-17 17:32:22 -0300

    Desafio:  Soma de números resolvido

---
+++ src/main/java/colletctions/list/pesquisa/Livro.java
@@ -0,0 +1,27 @@
+package colletctions.list.pesquisa;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    // a lista precisa de formatação para nao aparecer o local de memoria
+    @Override
+    public String toString() {
+        return "Livro [titulo=" + titulo + ", autor=" + autor + ", anoPublicacao=" + anoPublicacao + "]";
+    }
+}
